ac458ed1059c77cd7ca011bbff24e22f003a8054
Junior Mendes <junior.developer91@gmail.com>
2023-12-02 11:48:03 -0300

    WIP

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://controlxpress.com/dashboard-facul/css/style.css">
     <title>Dashboard | WebSai | Fatec</title>
 </head>
 
@@ -346,8 +346,8 @@
 
     </div>
 
-    <script src="/js/teste.js"></script>
-    <script src="/js/apexcharts.js"></script>
+    <script src="https://controlxpress.com/dashboard-facul/js/teste.js"></script>
+    <script src="https://controlxpress.com/dashboard-facul/js/apexcharts.js"></script>
     <script>
         var dataSexo = {{ Illuminate\Support\Js::from($dataSexo) }};
         var dataInfraestrutura = {{ Illuminate\Support\Js::from($dataInfraestrutura) }};
@@ -372,7 +372,7 @@
             return ((item * 100) / totalConhecimento).toFixed(2);
         });
     </script>
-    <script src="/js/app.js"></script>
+    <script src="https://controlxpress.com/dashboard-facul/js/app.js"></script>
 </body>
 
 </html>
